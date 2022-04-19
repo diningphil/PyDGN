69f08175a5494e594575e96f89c1923657c8b25b
Federico Errica <federico.errica@phd.unipi.it>
2022-04-19 14:01:25 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2022, Federico Errica'
 author = 'Federico Errica'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.5'
+release = '1.0.6'
 
 
 # -- General configuration ---------------------------------------------------
