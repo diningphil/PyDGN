31eb4d0b92be05a5b37b82d90283f58bc9fbca35
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2022-06-27 23:39:58 +0530

    feat(plotter): Remove W&B Code from main Plotter Class

--- pydgn/training/callback/plotter.py
+++ pydgn/training/callback/plotter.py
@@ -15,27 +15,14 @@ class Plotter(EventHandler):
     Args:
         exp_path (str): path where to store the Tensorboard logs
         use_wandb (bool): whether to upload tensorboard to wandb
-        keargs (dict): additional arguments that may depend on the plotter
+        kwargs (dict): additional arguments that may depend on the plotter
     """
-    def __init__(self, exp_path: str, use_wandb: bool, **kwargs: dict):
+    def __init__(self, exp_path: str, **kwargs: dict):
         super().__init__()
         self.exp_path = exp_path
 
         if not os.path.exists(Path(self.exp_path, TENSORBOARD)):
             os.makedirs(Path(self.exp_path, TENSORBOARD))
-        if use_wandb:
-            try:
-                import wandb
-                self._wandb = wandb
-            except ImportError:
-                raise ImportError(
-                    "To use the Weights and Biases Logger please install wandb."
-                    "Run `pip install wandb` to install it."
-                )
-            self._wandb.tensorboard.patch(root_logdir=self.exp_path, tensorboard_x=False, pytorch=True)
-            # Initialize a W&B run 
-            if self._wandb.run is None:
-                self._wandb.init(project="PyDGN")
         self.writer = SummaryWriter(log_dir=Path(self.exp_path, 'tensorboard'))
 
 
