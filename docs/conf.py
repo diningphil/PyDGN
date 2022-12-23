3a7ae6fa321490b36d9230ed2edf156ffe863b7c
Federico Errica <f.errica@protonmail.com>
2022-12-23 10:04:59 +0100

    changed version in files

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = "2022, Federico Errica"
 author = "Federico Errica"
 
 # The full version, including alpha/beta/rc tags
-release = "1.3.0.post1"
+release = "1.3.0.post2"
 
 
 # -- General configuration ---------------------------------------------------
